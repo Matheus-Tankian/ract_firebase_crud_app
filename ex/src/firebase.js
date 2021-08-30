50270c72a54511d221dd1bf0651033a02c3f61ea
Matheus Vitor de Oliveira Cruz <61215783+Matheus-Tankian@users.noreply.github.com>
2021-08-30 14:05:06 -0300

    Update firebase.js

--- ex/src/firebase.js
+++ ex/src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBVc7QUmvdM-lwfEqiAYLFbv9pQ2f3qEPE",
-    authDomain: "fb-crud-react-d403c.firebaseapp.com",
-    projectId: "fb-crud-react-d403c",
-    storageBucket: "fb-crud-react-d403c.appspot.com",
-    messagingSenderId: "87609536064",
-    appId: "1:87609536064:web:e723cd64592ae1014fb9a5"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxx.firebaseapp.com",
+    projectId: "xxxxxxxxxxxxxxx
+    storageBucket: "xxxxxxxxxxxxxxxxxxx"
+    messagingSenderId: "xxxxxxxxxxxxxxxxxx
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
   };
   // Initialize Firebase
 const fb = firebase.initializeApp(firebaseConfig);
